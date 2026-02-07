c089c86b4ad15fee828e565011419fb4ff074fa6
Aleksandr Nizamov <alexandr.nizamov@mail.ru>
2026-02-08 01:47:07 +0500

    Метод по получению списка тренировок за определенный день

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -28,6 +28,16 @@ public class Timetable {
 
     public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        TreeMap<TimeOfDay, List<TrainingSession>> dayListTreeMap = timetable.get(dayOfWeek);
+        if (dayListTreeMap == null) {
+            return new ArrayList<>();
+        }
+
+        List<TrainingSession> trainingSessionList = new ArrayList<>();
+        for (TimeOfDay time : dayListTreeMap.navigableKeySet()) {
+            trainingSessionList.addAll(dayListTreeMap.get(time));
+        }
+        return trainingSessionList;
     }
 
     public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
